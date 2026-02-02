4342d4516d3b65f2b75004e6be9c4b3232f2afb6
Hao Le <149852209+haole2k4@users.noreply.github.com>
2026-02-02 23:16:29 +0700

    Update resignfornewkernel.sh for kernel 6.18.8
    Added commands to verify and create initramfs for the new kernel version.

--- resignfornewkernel.sh
+++ resignfornewkernel.sh
@@ -1,10 +1,10 @@
 rpm -qa | grep cachyos
 
 sudo dnf remove \
-kernel-cachyos-6.18.5-1.fc43.x86_64 \
-kernel-cachyos-core-6.18.5-1.fc43.x86_64 \
-kernel-cachyos-modules-6.18.5-1.fc43.x86_64 \
-kernel-cachyos-modules-extra-6.18.5-1.fc43.x86_64
+  kernel-cachyos-6.18.5-1.fc43.x86_64 \
+  kernel-cachyos-core-6.18.5-1.fc43.x86_64 \
+  kernel-cachyos-modules-6.18.5-1.fc43.x86_64 \
+  kernel-cachyos-modules-extra-6.18.5-1.fc43.x86_64
 
 sudo openssl x509 \
   -inform DER \
@@ -16,6 +16,12 @@ sudo sbsign \
   --cert /etc/pki/akmods/certs/public_key.pem \
   /boot/vmlinuz-6.18.6-cachyos1.fc43.x86_64 \
   --output /boot/vmlinuz-6.18.6-cachyos1.fc43.x86_64.signed
+
+sbverify --list /boot/vmlinuz-6.18.8-cachyos1.fc43.x86_64
+
+sudo dracut -f \
+  /boot/initramfs-6.18.8-cachyos1.fc43.x86_64.img \
+  6.18.8-cachyos1.fc43.x86_64
 
 sudo mv \
   /boot/vmlinuz-6.18.6-cachyos1.fc43.x86_64.signed \
