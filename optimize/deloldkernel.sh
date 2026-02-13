d37b3c81277c4bbf311ca0c33b551dd54751bd73
haole2k4 <ahihihaodepzai@gmail.com>
2026-02-13 14:02:01 +0700

    Add script to remove old kernel versions

---
+++ optimize/deloldkernel.sh
@@ -0,0 +1,9 @@
+sudo dnf remove \
+kernel-6.18.8-200.fc43.x86_64 \
+kernel-core-6.18.8-200.fc43.x86_64 \
+kernel-modules-6.18.8-200.fc43.x86_64 \
+kernel-modules-core-6.18.8-200.fc43.x86_64 \
+kernel-modules-extra-6.18.8-200.fc43.x86_64 \
+kernel-devel-6.18.8-200.fc43.x86_64 \
+kmod-nvidia-6.18.8-200.fc43.x86_64-580.119.02-1.fc43.x86_64
+# lenh cho kernel 6.18.8-200.fc43.x86_64, neu co kernel moi hon thi thay doi so kernel trong lenh tren
